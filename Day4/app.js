d2dd666573c7ce5135bcf3ab68b3938813dd2857
Akshay Jain <akshayjain1999.aj18@gmail.com>
2021-01-30 12:00:00 +0530

    Day 4 app reorganized

--- Day4/app.js
+++ Day4/app.js
@@ -1,45 +1,47 @@
 const express= require('express');
 const app=express();
-const mongoose = require("mongoose");
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require("connect-flash");
-const indexRoutes= require("./routes/index");
+const request=require('request')
 const dotenv = require('dotenv');
 dotenv.config();
-console.log(`Your port is ${process.env.PORT}`); // 8626
-//Using Mongoose
-mongoose.connect('mongodb://localhost/DemuxApp');
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
-//Middleware
-app.use(flash());
-app.use(cookieParser());
 app.set("view engine", "ejs");
 app.use('/public', express.static('public'));
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-app.use(session({
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    secret: 'shhhh, very secret lubba wubba dubba etc'
-}));
-//To have a global variable across
-app.use(function(req, res, next) {
-    if (req.session.isLoggedIn) {
-        res.locals.currentUser = req.session.user;
-    } else {
-        res.locals.currentUser = null;
-    }
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
-    next();
+app.get("/", (req, res)=>{
+    res.render("Home");
 });
-//Routes
-app.use("/", indexRoutes);
+app.get("/result", (req, res)=>{
+    const query = req.query.search;
+    const url = "http://www.omdbapi.com/?apikey=cfd672ef&s=" + query;
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body)
+            //console.log(data);
+            if(data.Response==='False'){
+                res.send("Movie Not Found");
+            }else{
+                res.render("Result", {data:data});    
+            }
+        }else{
+            res.send('Error');
+        }
+    });
+});
+app.get("/result/:id", (req, res)=>{
+    const url = "http://www.omdbapi.com/?apikey=cfd672ef&i=" + req.params.id;
+    request(url, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            const data = JSON.parse(body)
+            //console.log(data);
+            if(data.Response==='False'){
+                res.send("Movie Not Found");
+            }else{
+                //res.send(data);
+                res.render("Info", {movie: data});    
+            }
+        }else{
+            res.send('Error');
+        }
+    });
+});
 app.get("*", (req, res)=>{
     res.send("Some Error");
 });
